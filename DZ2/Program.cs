307545b777aa74f9805a95a1d0601a5dd6ba2db1
Ivan <375291073707ls@gmail.com>
2022-07-12 21:55:15 +0300

    Добавили решенеи второй задачи

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Чтобы узнать какое число большее, а какое меньшее");
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n > m)
+{    Console.WriteLine("Число" +n+ "большее");
+     Console.WriteLine("Число" +m+ "меньшее");
+}
+if(n == m)
+{
+     Console.WriteLine("Число" +n+ "равно Числу" +m);
+}
+else
+{
+     Console.WriteLine("Число" +m+ "большее ");
+     Console.Write("Число" +n+ "меньшее ");
+}
